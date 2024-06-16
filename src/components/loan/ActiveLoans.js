51eba2b00fddb37b0e86742d5d161334a3ca7af2
Jairo Aguilar <jairoaguilar1219@gmail.com>
2024-06-15 21:15:31 -0600

    active hook

--- src/components/loan/ActiveLoans.js
+++ src/components/loan/ActiveLoans.js
@@ -37,7 +37,7 @@ const ActiveLoans = () => {
 
     useEffect(() => {
         fetchActiveLoans();
-    }, []);
+    }, [activeLoans]);
 
     const fetchBorrowerDetails = async (borrower) => {
         if (!borrowerDetails[borrower]) {
